7ad2da48fc09bb42c855f26c34d9f7066e66e474
Kaptian Core <kaptiancore@gmail.com>
2021-04-10 21:44:19 +1000

    Update weapon_taliban_flag_swep.lua

--- cg_ctf/lua/weapons/weapon_taliban_flag_swep.lua
+++ cg_ctf/lua/weapons/weapon_taliban_flag_swep.lua
@@ -30,34 +30,34 @@ SWEP.WElements = {
 SWEP.ViewModel = "models/weapons/c_bugbait.mdl"
 SWEP.WorldModel = "models/weapons/c_bugbait.mdl"
 
-function SWEP:PrimaryAttack()
-    -- Checks if we have enough ammo to shoot
-    if (self:CanPrimaryAttack() == false) then return end
+-- function SWEP:PrimaryAttack()
+--     -- Checks if we have enough ammo to shoot
+--     if (self:CanPrimaryAttack() == false) then return end
 
-    -- weapon_base: SWEP:ShootBullet(damage, numberOfBullets, aimcone, ammoType, force, tracer)
-    -- Shoots a bullet, handles player/weapon animations & applies recoil
-    self:ShootBullet(
-        self.Primary.Damage,
-        1,
-        self.Primary.Cone,
-        self.Primary.Ammo
-    )
+--     -- weapon_base: SWEP:ShootBullet(damage, numberOfBullets, aimcone, ammoType, force, tracer)
+--     -- Shoots a bullet, handles player/weapon animations & applies recoil
+--     self:ShootBullet(
+--         self.Primary.Damage,
+--         1,
+--         self.Primary.Cone,
+--         self.Primary.Ammo
+--     )
 
-    self:TakePrimaryAmmo(1)
+--     self:TakePrimaryAmmo(1)
 
-    self:EmitSound(self.Primary.Sound)
+--     self:EmitSound(self.Primary.Sound)
 
-    self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-end
+--     self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
+-- end
 
 
-function SWEP:SecondaryAttack()
-    if(!self:CanSecondaryAttack()) then return end
+-- function SWEP:SecondaryAttack()
+--     if(!self:CanSecondaryAttack()) then return end
 
-    if SERVER then
-        self:GetOwner():ChatPrint("Right click!")
-    end
+--     if SERVER then
+--         self:GetOwner():ChatPrint("Right click!")
+--     end
 
-    -- Delay when the gun can next be fired (secondary attack)
-    self:SetNextSecondaryFire(CurTime() + self.Secondary.Delay)
-end
+--     -- Delay when the gun can next be fired (secondary attack)
+--     self:SetNextSecondaryFire(CurTime() + self.Secondary.Delay)
+-- end

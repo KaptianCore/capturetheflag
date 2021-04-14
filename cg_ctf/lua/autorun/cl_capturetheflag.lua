f470e26a1131f8cd6314b4684e1895b03a3719aa
Kaptian Core <kaptiancore@gmail.com>
2021-04-14 13:45:41 +1000

    Comment Out Stuff Relating To Changing The Body Group Of The Model
    Due to the fact that I don't think cap will have time to do the body group model the flag will just be there as the entity itself

--- cg_ctf/lua/autorun/cl_capturetheflag.lua
+++ cg_ctf/lua/autorun/cl_capturetheflag.lua
@@ -1,12 +1,12 @@
 include('sh_capturetheflag.lua')
 include('sv_capturetheflag.lua')
-local team_ents = {
-	[1] = "us_flag"
-	[2] = "taliban_flag"
-}
+-- local team_ents = {
+-- 	[1] = "us_flag"
+-- 	[2] = "taliban_flag"
+-- }
 local function HandleFlagDropped(len, ply)
 	chat.AddText(unpack(net.ReadTable()))
-	local team_flag = ents.FindByName(team_ents[net.ReadInt(16)]) 	
+	-- local team_flag = ents.FindByName(team_ents[net.ReadInt(16)]) 	
 	-- Re Enable Body Group For Team's Flag
 	-- team_flag:SetBodygroup(0, 0) -- may have to set it to 1 idk	
 end
@@ -14,7 +14,7 @@ net.Receive("FlagDropped", HandleFlagDropped)
 
 local function HandleFlagTaken(len, ply)
 	chat.AddText(unpack(net.ReadTable()))
-	local team_flag = ents.FindByName(team_ents[net.ReadInt(16)])
+	-- local team_flag = ents.FindByName(team_ents[net.ReadInt(16)])
 	-- Disable Body Group For Team's Flag
     -- team_flag:SetBodygroup(0, 1) -- may have to set it to 0 idk	 	
 end
@@ -22,7 +22,7 @@ net.Receive("FlagTaken", HandleFlagTaken)
 
 local function HandleFlagCaptured(len, ply)
 	chat.AddText(unpack(net.ReadTable()))
-	local team_flag = ents.FindByName(team_ents[net.ReadInt(16)])
+	-- local team_flag = ents.FindByName(team_ents[net.ReadInt(16)])
 	-- Re Enable Body Group For Team's Flag
 	-- team_flag:SetBodygroup(0, 0) -- may have to set it to 1 idk	
 end
